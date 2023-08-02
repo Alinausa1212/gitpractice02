7e744a5cb771d2c2b7739fbc917aa369cf64128a
Alinausa1212 <alinausa1212@gmail.com>
2023-08-01 21:10:50 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello I am here");
         System.out.println("How are you");
+        System.out.println("New feature added");
+        System.out.println("New feature in progress");
     }
 }
